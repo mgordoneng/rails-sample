059f28611d4bc20109ad72ee3270d115645d1cc2
Marc Gordon <mjg31337@gmail.com>
2013-03-31 15:11:28 -0700

    setup basic grails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codesample::Application.config.secret_token = '6fd7e7fe1a339351c62cad8e819785642c34637c71da7709b7fad16575859fcfa5123bb23ab45e19b31a61e4615c67d284a5eaeccbc8731d10725290c6160f9b'
